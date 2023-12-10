5190e24076bd605b03c09ddc875fde436854fe51
Luca Salvarani <lucasalvarani99@gmail.com>
2023-12-11 00:06:51 +0100

    fix: Fix error 404 in Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lukesavefrogs.github.io',
+  base: IS_PRODUCTION ? '/WeddingWebsite' : '',
   integrations: [tailwind()],
   prefetch: {
     // Enable prefetching for all links, including those without the
