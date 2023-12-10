f4dbe20bd74ff6f00f13c3ab70d03cd0f772d307
Luca Salvarani <lucasalvarani99@gmail.com>
2023-12-11 00:41:47 +0100

    refactor(config): Add trailing slashes to urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const IS_PRODUCTION = import.meta.env.PROD;
 
 // https://astro.build/config
 export default defineConfig({
-  site: IS_PRODUCTION ? 'https://lukesavefrogs.github.io' : undefined,
-  base: IS_PRODUCTION ? '/WeddingWebsite' : undefined,
+  site: IS_PRODUCTION ? 'https://lukesavefrogs.github.io/' : undefined,
+  base: IS_PRODUCTION ? '/WeddingWebsite/' : undefined,
   integrations: [tailwind()],
   prefetch: {
     // Enable prefetching for all links, including those without the
