a5a33aa8e28261d626a27acfb39b59e93f6e61ca
Luca Salvarani <lucasalvarani99@gmail.com>
2023-12-11 00:09:49 +0100

    refactor(config): Unset `site` and `base` in dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lukesavefrogs.github.io',
-  base: IS_PRODUCTION ? '/WeddingWebsite' : '',
+  site: IS_PRODUCTION ? 'https://lukesavefrogs.github.io' : undefined,
+  base: IS_PRODUCTION ? '/WeddingWebsite' : undefined,
   integrations: [tailwind()],
   prefetch: {
     // Enable prefetching for all links, including those without the
