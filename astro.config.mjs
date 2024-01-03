d5999ff9b3bdfa246c07c34332d5798c3919e199
Luca Salvarani <lucasalvarani99@gmail.com>
2024-01-03 23:41:54 +0100

    chore(deps): Add alpine.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-
+import alpinejs from "@astrojs/alpinejs";
 const IS_PRODUCTION = import.meta.env.PROD;
+
 
 // https://astro.build/config
 export default defineConfig({
   site: IS_PRODUCTION ? 'https://lukesavefrogs.github.io/' : undefined,
   base: IS_PRODUCTION ? '/WeddingWebsite/' : undefined,
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   prefetch: {
     // Enable prefetching for all links, including those without the
     // `data-astro-prefetch` attribute.
